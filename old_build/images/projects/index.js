4d403038f32c233d53d12100b2a0203c27989f8c
aksinghania <akshtsinghnia@gmail.com>
2025-04-12 21:36:53 +0530

    added

---
+++ old_build/images/projects/index.js
@@ -0,0 +1,5 @@
+import Skillstrainer from "./skillstrainer.png";
+import Gameoflife from "./gameoflife.png";
+import DebateHouse from "./debatehouse.png";
+import SightSense from "./sightsense.png";
+export { Skillstrainer, Gameoflife, DebateHouse, SightSense };
